6bb63b1d98d92f9260197a5802fdfb1ae4f63e61
noobfranco <noobfranco09@gmail.com>
2025-06-25 12:40:52 -0500

    se termina la función para verificar di un código qr es válido o no, usé die para controlar los errores pero este se debe cambiar cuando todo funcione bie, por trow exception

--- functions/leerQr.php
+++ functions/leerQr.php
@@ -1,22 +1,41 @@
 <?php
 require '../models/mySql.php';
-require './funcionSanitizar.php';
 function verificarQr($codigo)
 {
 
-    if (!isset($codigo) && empty($codigo)) {
+    if (!isset($codigo) || empty($codigo)) {
         die("codigo inexistente");
     }
 
     $db = new MySQL();
     $db->conectar();
-    $conexion = $db->obtenerConexion();
-    $consulta = $conexion->prepare('select * from qr where codigo = ?');
-    $consulta->execute([$codigo]);
-    $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
-    if ($resultado) 
-    {
+    try {
+        $conexion = $db->obtenerConexion();
+        $consulta = $conexion->prepare('select * from qr where codigo = ?');
+        $consulta->execute([$codigo]);
+        $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException) {
+        die('error en la consulta');
+    }
 
+
+    if ($resultado) {
+        $fechaInicio = new DateTime($resultado['fechaInicio']);
+        $fechaFinal = new DateTime($resultado['fechaFinal']);
+        $fechaActual = new DateTime();
+        if ($resultado['estado'] == 1 && $fechaActual >= $fechaInicio && $fechaActual < $fechaFinal) {
+            try {
+                $consulta2 = $conexion->prepare('update qr set estado = 0 where codigo = :codigo');
+                $consulta2->execute(['codigo' => $codigo]);
+                return true;
+            } catch (PDOException) {
+                die('error en la consulta');
+            }
+
+
+        } else {
+            return false;
+        }
     } else {
         die('Qr inválido');
     }
